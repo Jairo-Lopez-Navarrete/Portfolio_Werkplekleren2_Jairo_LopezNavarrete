b21c0793b6ae602c68d4d6c7144e4bf60bb11565
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-05-30 10:43:15 +0200

    first commit

---
+++ homepage.js
@@ -0,0 +1,30 @@
+/*let icon = getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+}*/
+
+let icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+}
+
+/*function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+}
+ */
+
+/*const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+    document.body.classList.toggle('dark');
+})
+ */
+
+/*let darkToggle = document.querySelector('#darkToggle');
+
+darkToggle.addEventListener('change', ()=> {
+    document.body.classList.toggle('dark');
+});
+
+ */
